7da5aa1884b3db69713ed3e9ddcc4d0dd0d6c0ec
kaykhinewin <kkw19.win@gmail.com>
2023-05-16 14:16:33 +0630

    created ios UI

---
+++ LittleLemon/LittleLemon/Home.swift
@@ -0,0 +1,20 @@
+//
+//  Home.swift
+//  LittleLemon
+//
+//  Created by Kay Khine win on 16/5/23.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Home_Previews: PreviewProvider {
+    static var previews: some View {
+        Home()
+    }
+}
---
+++ LittleLemon/LittleLemon/Onboarding.swift
@@ -0,0 +1,20 @@
+//
+//  Onboarding.swift
+//  LittleLemon
+//
+//  Created by Kay Khine win on 16/5/23.
+//
+
+import SwiftUI
+
+struct Onboarding: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Onboarding_Previews: PreviewProvider {
+    static var previews: some View {
+        Onboarding()
+    }
+}
